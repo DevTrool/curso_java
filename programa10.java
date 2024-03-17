89cc7ffaf898efdca37a5d18277146b2dfba9517
Gabriel Rosa <89321808+DevTrool@users.noreply.github.com>
2024-03-17 19:22:23 -0300

    Create programa10.java

---
+++ programa10.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+//for
+public class programa10 {
+
+    public static void main(String[] args){
+        String nome = "trool";
+
+        //para cada um dos caracteres da string, imprima o caracter
+        for (char letra : nome.toCharArray()) {
+            System.out.println(letra);
+            //System.out.print(letra);
+        }
+    }
+}
